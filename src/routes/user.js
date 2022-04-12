401d1c22da8bf630adc6db27b4801ea2dcc41bbd
LuBly <lumenize@ajou.ac.kr>
2022-04-13 00:04:00 +0900

    push check

---
+++ src/routes/user.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const express = require('express')
+const { PrismaClient } = require('@prisma/client')
+
+const router = express.Router();
+const prisma = new PrismaClient();
+
+router.get("/", (req, res, next) => {
+	res.json({"hello": "hello"});
+});
+
+export default router;
