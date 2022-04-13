8f2a4f6c307a2a6d0849bf426ff5faabeab6fe80
Jeong Ga Hee <jghgahee12@gmail.com>
2022-04-13 13:58:03 +0900

    add routes about plan

--- src/routes/plan.js
+++ src/routes/plan.js
@@ -1,8 +1,16 @@
 import express from 'express';
-import { handleStar } from "../controllers/planController";
+import { route } from 'express/lib/application';
+import { createPlan, handleStar } from "../controllers/planController";
 
 const router = express.Router();
 
+router.post("/plan",createPlan);
+router.delete("/plan/:planId");
+router.patch("/plan/:planId");
+router.patch("/plan/check");
+router.get("/plan/total?date=${date}&userId=${userId}");
+router.get("/plan/week/average?date=${date}&userId=${userId}");
+router.get("/plan/month/average?date=${date}&userId=${userId}");
 router.post("/star", handleStar);
 router.get("/statistic?date=${date}&userId=${userId}");
 router.get("/week/total?date=${date}&userId=${userId}");
