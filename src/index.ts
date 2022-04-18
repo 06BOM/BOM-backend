5ef903a431baee53f14bce35ae7c9e843f988870
Brylimo <icj0103@gmail.com>
2022-04-18 14:08:45 +0900

    Add typescript 2

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import app from "./app";
-const PORT = process.env.PORT || 3000;
-
-const handleListening = () => {
-	console.log(`✅ Server is ready. http://localhost:${PORT}`);
-}
-
-app.listen(PORT, handleListening);
