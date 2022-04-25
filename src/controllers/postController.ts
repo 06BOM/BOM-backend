58cd05cf815bf6b4a87a1efacbd7315acab229b8
Jeong Ga Hee <jghgahee12@gmail.com>
2022-04-25 20:13:27 +0900

    implement deletePost api & soltingPostByCategory api

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -6,12 +6,31 @@ const prisma = new PrismaClient();
 
 export const deletePost = async (req: Request, res: Response, next: NextFunction): Promise<unknown> => {
 	const postId = parseInt(String(req.query.postId));
-    console.log("postId: ", postId);
 
 	try {
+        const result = await prisma.post.delete({
+            where: { postId }
+        })
 
         return res.json({ opcode: OPCODE.SUCCESS });
 
+    } catch(error) {
+        console.log(error);
+        next(error);
+    }
+}
+
+export const soltingPostByCategory = async (req: Request, res: Response, next: NextFunction): Promise<unknown> => {
+    const categoryId = parseInt(String(req.query.categoryId));
+
+    try {
+        const resultPosts = await prisma.post.findMany({
+            where: { categoryId },
+            orderBy: { createdAt: 'desc' }
+        })
+
+        return res.json({ opcode: OPCODE.SUCCESS, resultPosts });
+        
     } catch(error) {
         console.log(error);
         next(error);
--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { deletePost } from "../controllers/postController";
+import { deletePost, soltingPostByCategory } from "../controllers/postController";
 
 const router = express.Router();
 
 router.delete("/delete", deletePost);
+router.get("/category", soltingPostByCategory);
 
 
 export default router;
