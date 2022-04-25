094fdf24a325762adaab8c7153ed4dad9047e8fe
Brylimo <icj0103@gmail.com>
2022-04-25 21:21:13 +0900

    post update issue

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -17,3 +17,45 @@ export const deletePost = async (req: Request, res: Response, next: NextFunction
         next(error);
     }
 }
+
+export const updatePost = async (req: Request, res: Response, next: NextFunction): Promise<unknown> => {
+	// const postId = parseInt(String(req.body.postId));
+	const title = req.body.title;
+	const content = req.body.content;
+	const postKind = parseInt(String(req.body.postKind));
+	const anonymous = Boolean(req.body.anonymous);
+
+	//const postId2 = req.query.postId;
+
+	console.log("postId", postId2);
+	const postId = Number(postId2);
+	console.log("postId", postId);
+	console.log("title", title);
+	console.log("content", content);
+	console.log("postKind", postKind);
+	console.log("anonymous", anonymous);
+	try {
+		const postData = await prisma.post.findUnique({
+			where: {
+				postId
+			}
+		});
+		/*
+		const post = await prisma.post.update({
+			where: {
+				postId
+			},
+			data: {
+				title,
+				content,
+				postKind,
+				anonymous
+			},
+		});*/
+		console.log("post", postData);
+		return res.json({ opcode: OPCODE.SUCCESS, postData });
+	} catch(error) {
+		console.log(error);
+		next(error);
+	}
+}
--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { deletePost } from "../controllers/postController";
+import { deletePost, updatePost } from "../controllers/postController";
 
 const router = express.Router();
 
 router.delete("/delete", deletePost);
+router.patch("/", updatePost);
 
 
 export default router;
