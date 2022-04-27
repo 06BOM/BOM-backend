771b9da1b9663ea2d9bf9148138b29d046a4ae3a
Brylimo <icj0103@gmail.com>
2022-04-27 16:12:50 +0900

    Add GET /comment/reply?commentId=${commentId}

--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import { createComment, getCommentWithoutReply, createReply } from "../controllers/commentController";
+import { createComment, getCommentWithoutReply, createReply, getCommentWithReply } from "../controllers/commentController";
 
 const router = express.Router();
 
 router.post('', createComment);
 router.get('', getCommentWithoutReply);
 router.post('/reply', createReply);
-
+router.get('/reply', getCommentWithReply);
 
 export default router;
