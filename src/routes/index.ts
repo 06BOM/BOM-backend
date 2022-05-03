5d2f5625840e5b2ad79dc61f357b6805c171b129
LuBly <lumenize@ajou.ac.kr>
2022-05-04 00:36:00 +0900

    delete tools/createRepiPlan

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,9 @@
 import express from "express";
 import { Request, Response } from 'express';
-import { createRepiPlan } from "../tools";
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
 	res.send("Hello!!");
-	createRepiPlan(0, 0, 0, 1);
 });
 
 export default router;
