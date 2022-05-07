2b57aeca8fa9eadc7e679ced59b0566ee73c2344
Jeong Ga Hee <jghgahee12@gmail.com>
2022-05-07 20:23:57 +0900

    implement room_enter

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,7 +1,28 @@
 const socket = io();
 
+const welcome = document.getElementById("welcome");
+const form_welcome = welcome.querySelector("form");
+const ox = document.getElementById("ox");
 const button_o = document.querySelector('.o');
 const button_x = document.querySelector('.x');
+
+let roomName;
+ox.hidden = true;
+
+function showRoom() {
+    welcome.hidden = true;
+    ox.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName}`;
+};
+
+function handleRoomSubmit(event) {
+    event.preventDefault();
+    const input = form_welcome.querySelector("input");
+    socket.emit("enter_room", input.value, showRoom);
+    roomName = input.value;
+    input.value = "";
+}
 
 function handleOSubmit(event) {
 	event.preventDefault();
@@ -13,9 +34,16 @@ function handleXSubmit(event) {
 	socket.emit("ox", { userId: 1, ox: 'x'});
 }
 
+form_welcome.addEventListener("submit", handleRoomSubmit);
 button_o.addEventListener("click", handleOSubmit);
 button_x.addEventListener("click", handleXSubmit);
 
 socket.on("ox", (payload) => {
 	console.log(payload);
+});
+
+socket.on("welcome", (user, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName} (${newCount})`;
+    addMessage(`${user} arrived!`);
 });
