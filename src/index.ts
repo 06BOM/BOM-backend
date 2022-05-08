477871adb1f1eec56f27ab6740bb6f16a1728959
Brylimo <icj0103@gmail.com>
2022-05-08 21:33:32 +0900

    Add question socket event

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { Server } from "socket.io";
 import { doesNotMatch } from "assert";
 import { Socket } from "dgram";
 import { clearInterval } from "timers";
+import { Console } from "console";
 
 const PORT = process.env.PORT || 3000;
 
@@ -20,18 +21,25 @@ function countRoom(roomName){
 }
 
 let readyStorage = [];
+let arr = [];
+
 const question = [
-	{ oxQuestion: "이 앱의 이름은 다모여이다.", oxAnswer: "o" },
-	{ oxQuestion: "이 앱을 만든 조의 이름은 BOOM이다", oxAnswer: "x" },
-	{ oxQuestion: "이 앱을 만든 조는 6조이다.", oxAnswer: "o" },
-	{ oxQuestion: "토마토는 과일이 아니라 채소이다.", oxAnswer: "x" },
-	{ oxQuestion: "원숭이에게도 지문이 있다.", oxAnswer: "x" },
-	{ oxQuestion: "가장 강한 독을 가진 개구리 한마리의 독으로 사람 2000명 이상을 죽일 수 있다.", oxAnswer: "o" },
-	{ oxQuestion: "달팽이는 이빨이 있다", oxAnswer: "o" },
-	{ oxQuestion: "고양이는 잠을 잘 때 꿈을 꾼다", oxAnswer: "o" },
-	{ oxQuestion: "물고기도 색을 구분할 수 있다.", oxAnswer: "o" },
-	{ oxQuestion: "낙지의 심장은 3개이다", oxAnswer: "o" },
+	{ oxQuestion: "이 앱의 이름은 다모여이다.", oxAnswer: "o", explanation: "이 앱의 이름은 다모여가 맞다." },
+	{ oxQuestion: "이 앱을 만든 조의 이름은 BOOM이다", oxAnswer: "x", explanation: "이 앱을 만든 조의 이름은 BOM이다." },
+	{ oxQuestion: "이 앱을 만든 조는 6조이다.", oxAnswer: "o", explanation: "이 앱을 만든 조는 6조가 맞다." },
+	{ oxQuestion: "토마토는 과일이 아니라 채소이다.", oxAnswer: "o", explanation: "토마토는 채소이다." },
+	{ oxQuestion: "원숭이에게는 지문이 없다.", oxAnswer: "x", explanation: "원숭이에게도 지문이 있다." },
+	{ oxQuestion: "가장 강한 독을 가진 개구리 한마리의 독으로 사람 2000명 이상을 죽일 수 있다.", oxAnswer: "o", explanation: "아프리카에 사는 식인 개구리의 독성으로 2000명의 사람을 죽일 수 있다." },
+	{ oxQuestion: "달팽이는 이빨이 있다", oxAnswer: "o", explanation: "달팽이도 이빨이 있다." },
+	{ oxQuestion: "고양이는 잠을 잘 때 꿈을 꾼다", oxAnswer: "o", explanation: "고양이도 잠을 잘 때 꿈을 꾼다." },
+	{ oxQuestion: "물고기도 색을 구분할 수 있다.", oxAnswer: "o", explanation: "물고기도 색을 구분한다." },
+	{ oxQuestion: "낙지의 심장은 3개이다", oxAnswer: "o", explanation: "낙지의 심장은 3개이다." }
 ];
+
+for (let i = 0; i < question.length; i++)
+{
+	arr.push(0);
+}
 
 wsServer.on("connection", socket => {
 	socket["nickname"] = "Anon";
@@ -86,7 +94,26 @@ wsServer.on("connection", socket => {
     });
 
 	socket.on("question", (done) => {
-		done(question[0].oxQuestion);
+		let cnt = 0;
+
+		for (let i = 0; i < question.length; i++) // Question 문제를 다 전송했는지 확인
+		{
+			if (arr[i] === 1) {
+				cnt++;
+			}
+			if (cnt >= question.length) {
+				return; 
+			}
+		}
+
+		let index = Math.floor(Math.random() * 10);
+
+		while(arr[index]) {
+			index = Math.floor(Math.random() * 10);
+		}		
+
+		arr[index] = 1;
+		done(question[index].oxQuestion);
 	});
 });
 
