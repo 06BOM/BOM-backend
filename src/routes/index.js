9899e7c3ddb7edd31b5118aa89630a9e5038c3ce
Brylimo <icj0103@gmail.com>
2022-05-20 15:23:49 +0900

    CI/CD TEST

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
 	console.log("hello");
-	res.send("Hello!!");
+	res.send("Hello!! this is for ci/cd test!!!");
 });
 
 export default router;
