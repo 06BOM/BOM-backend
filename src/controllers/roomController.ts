38ee305c0c5a5c68fb5018923fb94db092ba5ff2
LuBly <lumenize@ajou.ac.kr>
2022-05-22 20:16:18 +0900

    add searchRoom API

--- src/controllers/roomController.ts
+++ src/controllers/roomController.ts
@@ -21,3 +21,25 @@ export const getRoomInformation = async (req: Request, res: Response, next: Next
         next(error);
     }
 }
+
+export const searchRoom = async (req: Request, res: Response, next: NextFunction): Promise<unknown> => {
+    
+    const search = String(req.query.search);
+    
+
+    try {
+        const resultRoom = await prisma.room.findMany({
+            where:{
+                roomName : {
+                    contains : search
+                }
+            }
+        })
+
+        return res.json({ opcode: OPCODE.SUCCESS, resultRoom })
+
+    } catch(error) {
+        console.log(error);
+        next(error);
+    }
+}
--- src/routes/room.ts
+++ src/routes/room.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { getRoomInformation,
+         searchRoom,
     
 } from "../controllers/roomController";
 
@@ -8,5 +9,6 @@ import { getRoomInformation,
 const router = express.Router();
 
 router.get("/", getRoomInformation);
+router.get("/search", searchRoom);
 
 export default router;
