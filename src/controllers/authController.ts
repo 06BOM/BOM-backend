ef10a2e4c565e8e804276c823b55a75830253abf
Jeong Ga Hee <jghgahee12@gmail.com>
2022-05-27 13:55:35 +0900

    add signin api $ password encode, decode func

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -37,6 +37,7 @@ const verifyPassword = async (password, userSalt, userPassword) => {
 export const signIn = async (req: Request, res: Response, next: NextFunction): Promise<unknown> => { 
 	let userInfo = {
 		emailId: String(req.body.emailId),
+		grade: req.body.grade,
 		password: "",
 		salt: "",
 		userName: String(req.body.userName),
@@ -64,55 +65,55 @@ export const signIn = async (req: Request, res: Response, next: NextFunction): P
 export const logIn = async (req: Request, res: Response, next: NextFunction): Promise<unknown> => { 
 	try {
 
-		// let existUser = true;
+		let existUser = true;
 	
-		// const { kakaoAccessToken } = req.body;
-		// const headers = {
-		// 	"Content-Type": "application/x-www-form-urlencoded",
-		// 	Authorization: "Bearer " + kakaoAccessToken
-		// };
-		// const response = await axios.get("https://kapi.kakao.com/v2/user/me", {
-		// 	headers,
-		// });
-		// let user = await prisma.user.findFirst({
-		// 	where: {
-		// 		AND: [
-		// 			{ platform: "kakao" },
-		// 			{ platformId: response.data.id }
-		// 		]
-		// 	}
-		// });
+		const { kakaoAccessToken } = req.body;
+		const headers = {
+			"Content-Type": "application/x-www-form-urlencoded",
+			Authorization: "Bearer " + kakaoAccessToken
+		};
+		const response = await axios.get("https://kapi.kakao.com/v2/user/me", {
+			headers,
+		});
+		let user = await prisma.user.findFirst({
+			where: {
+				AND: [
+					{ platform: "kakao" },
+					{ platformId: response.data.id }
+				]
+			}
+		});
 
-		// if (!user) {
-		// 	let userContent = {
-		// 		platform: 'kakao',
-		// 		platformId: String(response.data.id),
-		// 		nickname: String(response.data.properties.nickname),
-		// 		userName: String(response.data.properties.userName)
-		// 	}
+		if (!user) {
+			let userContent = {
+				platform: 'kakao',
+				platformId: String(response.data.id),
+				nickname: String(response.data.properties.nickname),
+				userName: String(response.data.properties.userName)
+			}
 			
-		// 	user = await prisma.user.create({
-		// 		data: userContent
-		// 	});
-		// 	existUser = false;
-		// }
+			user = await prisma.user.create({
+				data: userContent
+			});
+			existUser = false;
+		}
 
-		// const sessionId = await Sessions.createSession(user);
-		// const accessToken = Token.signJwt(
-		// 	{userId: user.userId, sessionId},
-		// 	"5s"
-		// );
-		// const refreshToken = Token.signJwt(
-		// 	{ sessionId },
-		// 	"1d"
-		// )
+		const sessionId = await Sessions.createSession(user);
+		const accessToken = Token.signJwt(
+			{userId: user.userId, sessionId},
+			"5s"
+		);
+		const refreshToken = Token.signJwt(
+			{ sessionId },
+			"1d"
+		)
 
-		// if (existUser === true) {
-		// 	return res.status(201).json({ accessToken, refreshToken});
-		// } else {
-		// 	return res.status(200).json({ accessToken, refreshToken});
-		// }
-		return
+		if (existUser === true) {
+			return res.status(201).json({ accessToken, refreshToken});
+		} else {
+			return res.status(200).json({ accessToken, refreshToken});
+		}
+		
     } catch(error) {
         console.log(error);
         next(error);
