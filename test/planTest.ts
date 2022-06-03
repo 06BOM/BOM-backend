f4c55ef8b34f8ea0378338c38a3d42ada9c198dd
Brylimo <icj0103@gmail.com>
2022-06-03 15:22:41 +0900

    Add issuing token process in local login

--- test/planTest.ts
+++ test/planTest.ts
@@ -4,8 +4,9 @@ import app from "../src/app";
 import should from 'should';
 import request from 'supertest';
 
+let auth = {};
+
 describe('plan controller test', () => {
-	let auth = {};
 	before(loginUser(auth));
 
 	describe('createPlan func test', () => {
@@ -16,6 +17,17 @@ describe('plan controller test', () => {
       //테스트 코드
     })    
   });
+
+  describe('pr', () => {
+	it('should return success json', (done) => {
+		request(app)
+			.get('/')
+			.expect(res => {
+				should(res).have.property('statusCode', 200);
+			}).end(done);
+	});
+  })
+
   /* 
   describe('getDailyStar func test', () => {
 	it('should return success json', (done) => {
@@ -33,13 +45,18 @@ describe('plan controller test', () => {
 });
 
 function loginUser(auth) {
-    return function() {
-		console.log("before hook");
-	}
-	/*return function(done) {
-        request
-            .post('/auth/login')
-            .send({
+	return function(done) {
+		request(app)
+		.post('/auth/login')
+		.send({
+			platform: 'local',
+            emailId: 'test@bombomtest.com',
+            password: 'Qlalfqjsgh06@'
+		})
+		.expect(res => {
+			should(res).have.property('statusCode', 200);
+		}).end(done);
+            /*.send({
 				platform: 'local',
                 email: 'test@bombomtest.com',
                 password: 'Qlalfqjsgh06@'
@@ -52,6 +69,6 @@ function loginUser(auth) {
 			//console.log(res);
             //auth.token = res.body.accessToken;
             return done();
-        }
-    };*/
+        }*/
+    }
 }
